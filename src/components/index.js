ab818c5095a1a9d3a8cf5f5c7f325b283ba3264d
suyog <03suyogpawar@gmail.com>
2026-03-23 01:20:06 +0530

    Updated selected components and reducers

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,7 @@ export { Footer } from "./layout/Footer";
 export { ProductCard } from './Element/ProductCard';
 export { Rating } from "./Element/Rating";
 
+export { DropdownLoggedOut } from "./Element/DropdownLoggedOut"
+export { DropdownLoggedIn } from "./Element/DropdownLoggedIn"
+
 export { ScrollTotop } from "./other/ScrollTotop"
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.png";
 import { Search } from "../Sections/Search";
+import { DropdownLoggedOut } from "../Element/DropdownLoggedOut";
 
 export const Header = () => {
   const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("darkMode")) || false);
   const [searchSection, setSearchSection] = useState(false);
+  const [ dropdown , setDropdown] = useState(false);
 
+  
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
     
@@ -33,11 +36,14 @@ export const Header = () => {
                       <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full ">0</span>
                     </span>                    
                   </Link>
-                  <span className="bi bi-person-circle cursor-pointer text-2xl text-gray-700 dark:text-white"></span>
+                  <span  onClick ={ () => setDropdown(!dropdown)}className="bi bi-person-circle cursor-pointer text-2xl text-gray-700 dark:text-white"></span>
+                  { dropdown && <DropdownLoggedOut/> }
               </div>
           </div>
       </nav>
       { searchSection && <Search setSearchSection={setSearchSection} /> }
+      
+
     </header>
   )
 }
